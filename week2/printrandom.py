da85979d5a6d5e6d8e2c7852406ddb082184f8d4
Yrjan Skrimstad <yrjans@gmail.com>
2015-09-01 10:53:41 +0200

    Added printrandom.py and temperatureconverter.py.

---
+++ week2/printrandom.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+from random import uniform
+
+print "{:.4f}".format(uniform(-1, 1))
---
+++ week2/temperatureconverter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+if len(sys.argv) < 2:
+    print "USAGE: python {0} [FAHRENHEIT]".format(sys.argv[0])
+    print "Converts temperature in fahrenheit to temperature in celsius."
+    sys.exit()
+
+fahrenheit = float(sys.argv[1])
+
+celsius = (fahrenheit - 32) * 5/9.
+print "{0:.1f} fahrenheit is {1:.1f} celsius.".format(fahrenheit, celsius)
