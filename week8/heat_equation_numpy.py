e5bf98202a00822af6eebaccb845ce6b25c50aa0
Yrjan Skrimstad <yrjans@gmail.com>
2015-10-22 16:45:54 +0200

    Modified heat_equation.py and added heat_equation_numpy.py

--- week8/heat_equation_numpy.py
+++ week8/heat_equation_numpy.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def heat_equation(t0, t1, dt, n, m, u, f, nu):
+def heat_equation_np(t0, t1, dt, n, m, u, f, nu):
     """
     Solves heat equation where t0 is start time, t1 is end time,
     dt is time step, n is rectangle is rectangle width, m is rectangle height,
@@ -14,20 +15,14 @@ def heat_equation(t0, t1, dt, n, m, u, f, nu):
     """
     t = t0
     while t < t1:
-        # Init u_n to zero.
-        u_n = [([0] * n) for _ in xrange(m)]
-        # u_n = [[0] * n] * m
-        for i in xrange(1, m-1):
-            for j in xrange(1, n-1):
-                u_n[i][j] = u[i][j] \
-                            + dt*(nu*u[i-1][j] + nu*u[i][j-1] - 4*nu*u[i][j] +
-                                  nu*u[i][j+1] + nu*u[i+1][j] + f[i][j])
-        u = u_n
+        u[1:-1, 1:-1] += dt*(nu*u[:-2, 1:-1] + nu*u[1:-1, :-2] -
+                             4*nu*u[1:-1, 1:-1] + nu*u[1:-1, 2:] +
+                             nu*u[2:, 1:-1] + f[1:-1, 1:-1])
         t += dt
     return u
 
 
-def heat_equation_plot(t0, t1, dt, n, m, u, f, nu):
+def heat_equation_plot_np(t0, t1, dt, n, m, u, f, nu):
     """
     Solves and plots heat equation where t0 is start time, t1 is end time,
     dt is time step, n is rectangle is rectangle width, m is rectangle height,
@@ -38,7 +33,7 @@ def heat_equation_plot(t0, t1, dt, n, m, u, f, nu):
     fig, axes = plt.subplots(nrows=1, ncols=2)
     axes[0].imshow(u, plt.get_cmap("gray"))
 
-    u = heat_equation(t0, t1, dt, n, m, u, f, nu)
+    u = heat_equation_np(t0, t1, dt, n, m, u, f, nu)
 
     # Plot t1 plot and colorbar.
     im = axes[1].imshow(u, plt.get_cmap("gray"))
@@ -52,7 +47,8 @@ if __name__ == '__main__':
     dt = 0.1
     n = 50
     m = 100
-    u = [([0] * n) for y in xrange(m)]
-    f = [([1] * n) for y in xrange(m)]
+    u = np.zeros((m, n))
+    f = np.empty((m, n))
+    f.fill(1)
     nu = 1.
-    heat_equation_plot(t0, t1, dt, n, m, u, f, nu)
+    heat_equation_plot_np(t0, t1, dt, n, m, u, f, nu)
